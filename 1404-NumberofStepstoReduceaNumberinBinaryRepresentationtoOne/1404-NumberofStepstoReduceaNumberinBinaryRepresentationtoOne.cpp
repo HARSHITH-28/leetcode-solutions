18808a103d65c866ab4a86216b732df08b59f905
HARSHITH C <harshithc2828@gmail.com>
2026-02-26 15:57:13 +0530

    File updated at 2/26/2026, 3:57:12 PM

--- 1404-NumberofStepstoReduceaNumberinBinaryRepresentationtoOne/1404-NumberofStepstoReduceaNumberinBinaryRepresentationtoOne.cpp
+++ 1404-NumberofStepstoReduceaNumberinBinaryRepresentationtoOne/1404-NumberofStepstoReduceaNumberinBinaryRepresentationtoOne.cpp
@@ -1,18 +1,22 @@
-// Last updated: 2/26/2026, 12:40:58 PM
+// Last updated: 2/26/2026, 3:57:12 PM
 1class Solution {
 2public:
 3    int numSteps(string s) {
-4        int count=0;
-5        while(s.size()!=1){
-6            if(s.back()=='0') s.pop_back();
-7            else{
-8                int ind=s.size()-1;
-9                while(ind>=0 && s[ind]!='0') s[ind--]='0';
-10                if(ind>=0) s[ind]='1';
-11                else s.insert(0,1,'1');
-12            }
-13            count++;
-14        }
-15        return count;
-16    }
-17};
+4        int i=0;
+5        int cnt=0;
+6        while(s.size()!=1){
+7            if(s.back()!='0'){
+8                for(i=s.size()-1;i>=0 && s[i]!='0';i--){
+9                    s[i]='0';
+10                }
+11                if(i>=0) s[i]='1';
+12                else s.insert(0,1,'1');
+13            }
+14            else{
+15                s.pop_back();
+16            }
+17            cnt++;
+18        }
+19        return cnt;
+20    }
+21};
