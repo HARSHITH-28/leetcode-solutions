da3da149a275d87054df5136393e77aa02b4171a
HARSHITH C <harshithc2828@gmail.com>
2026-05-19 08:09:29 +0530

    Create 2540-MinimumCommonValue

---
+++ 2540-MinimumCommonValue/2540-MinimumCommonValue.cpp
@@ -0,0 +1,14 @@
+// Last updated: 5/19/2026, 8:09:28 AM
+1class Solution {
+2public:
+3    int getCommon(vector<int>& nums1, vector<int>& nums2) {
+4        int n=nums1.size(),m=nums2.size();
+5        int i=0,j=0;
+6        while(i<n and j<m){
+7            if(nums1[i]==nums2[j]) return nums1[i];
+8            if(nums1[i]<nums2[j]) i++;
+9            else j++;
+10        }
+11        return -1;
+12    }
+13};
