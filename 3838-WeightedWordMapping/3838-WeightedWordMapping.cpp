d95bbe208cb42d35f38f43e8d0da166709912d50
HARSHITH C <harshithc2828@gmail.com>
2026-06-13 12:08:07 +0530

    Create 3838-WeightedWordMapping

---
+++ 3838-WeightedWordMapping/3838-WeightedWordMapping.cpp
@@ -0,0 +1,18 @@
+// Last updated: 6/13/2026, 12:08:06 PM
+1class Solution {
+2public:
+3    string mapWordWeights(vector<string>& words, vector<int>& weights) {
+4        string res;
+5        unordered_map<char,int> mp;
+6        int i=25;
+7        for(char c='a';c<='z';c++) mp[i--]=c;
+8        for(string s:words){
+9            int w=0;
+10            for(char c:s){
+11                w+=weights[c-'a'];
+12            }
+13            res.push_back(mp[w%26]);
+14        }
+15        return res;
+16    }
+17};
